14ab55929cef1eb3b45e8ed3bf64bc3b7c1a4e7a
Soumyadip Maity <22053029@kiit.ac.in>
2024-12-22 17:25:46 +0530

    Refactor Page component to include HeroSection and clean up imports

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,16 @@
-import React from 'react'
-// import Navbar from '@/components/LandingPage/Navbar'
-// import WhyUs from '@/components/LandingPage/WhyUs'
-// import HeroSection from '@/components/LandingPage/HeroSection';
-
-function Page() {
-  return (
-    <div >
-
-      {/* <HeroSection /> */}
-      {/* <WhyUs /> */}
-import Navbar from '@/components/Menubar/Header'
-import WhyUs from '@/components/LandingPage/WhyUs'
-import Footer from '@/components/Menubar/Footer'
+import WhyUs from "@/components/LandingPage/WhyUs";
+import Footer from "@/components/Menubar/Footer";
+import HeroSection from "@/components/LandingPage/HeroSection";
+import Faq from "@/components/LandingPage/faq";
 
 function Page() {
   return (
     <div>
-      <Navbar />
-      {/* Landing Page */}
+      <HeroSection />
+      {/* <Faq /> */}
       <WhyUs />
       <Footer />
     </div>
-    
   );
 }
 
